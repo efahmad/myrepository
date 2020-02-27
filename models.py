619f0c177eaa5f37273816671c112b18826337a5
ohashemzadeh <61495114+ohashemzadeh@users.noreply.github.com>
2020-02-27 16:25:22 +0330

    added version 3

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-## JUST for testing version 2
+## JUST for testing version 3
 
 class Product(models.Model):
     code = models.CharField(unique=True, max_length=10)
     name = models.CharField(max_length=100)
-    price = models.PositiveIntegerField()  # ولیدیتور رو نذار
+    price = models.PositiveIntegerField()
     inventory = models.PositiveIntegerField(default=0)   #validators=[MinValueValidator(0)]
 
     # def __init__(self):
@@ -48,7 +48,6 @@ class Customer(models.Model):
             raise Exception('کمبود اعتبار: مبلغ درخواست شده، بیش از اعتبار موجود می باشد. لطفا ابتدا حساب خود را شارژ نموده و مجددا تلاش نمایید.')
 
 
-
 class Order(models.Model):
     # Status values. DO NOT EDIT
     STATUS_SHOPPING = 1
@@ -76,18 +75,48 @@ class Order(models.Model):
         o.status = Order.STATUS_SHOPPING    # وضعیت در حال خرید
         o.customer = customer
         o.save()
+        return (o)      # Order
 
     def add_product(self, product, amount):
-        pass
+        if (amount == 0): #امکان افزودن صفر عدد از یک کالا به سبد خرید مشتری وجود ندارد.
+            raise Exception('امکان افزودن صفر عدد از یک کالا به سبد خرید مشتری وجود ندارد.')
+        elif (product.inventory < amount):  #مشتری نمی‌تواند کالایی را بیش از ظرفیت موجود در فروشگاه به سبد خرید خود اضافه کند.
+            raise Exception('کمبود کالا')
+        else:
+            self.total_price += product.price * amount
+            self.save()
+            # آیا باید inventory محصول را نیز همزمان از Product کم کنیم ؟
 
     def remove_product(self, product, amount=None):
-        pass
+        if (amount == None):        # اگر تعدادی مشخص نشده بود، کلا Object OrderRow را Delete کن.
+            OrderRow.objects.filter(product__orderrow__order_id=product.id)[0].delete()
+        elif OrderRow.objects.filter(product__orderrow__order_id=product.id).count()>0:   # اگر محصول وارد شده در تابع، در این سفارش موجود نبود :
+            raise Exception('عدم وجود کالا جهت کم کردن از سبد خرید')
+        else:
+            self.total_price -= product.price*amount
+            self.save()
 
     def submit(self):
-        pass
+        #در زمان ثبت سفارش، اولا باید بررسی شود که از تمام کالاهای مورد سفارش مشتری به اندازه کافی در فروشگاه وجود داشته باشد،
+        # بحث چک کردن میزان موجودی، در تابع addproduct بررسی می شود.
 
-    def cancel(self):
+        # ثانیا باید مشتری به اندازی جمع مبلغ سفارش خود اعتبار داشته باشد
+        # یعنی این شرط برقرار باشد  self.total_price =<  اعتبار مشتری  customer.balance
+        if self.total_price >= Customer.objects.get(id = self.customer_id ).balance :   # اگرکل مبلغ سفارش بیش از اعتبار مشتری بود
+            self.status = self.STATUS_SHOPPING #در صورتی که ثبت سفارش با موفقیت انجام نشود، سفارش «در حال خرید» باقی می‌ماند و موجودی کالاها و اعتبار مشتری نیز دست نمی‌خورد.
+            self.save()
+            raise Exception('کمبود اعتبار')
+        else:
+            self.status = self.STATUS_SUBMITTED  # سفارش «ثبت‌شده» تلقی می‌شود و پول آن از حساب مشتری کاسته شده و موجودی کالاها نیز از موجودی فروشگاه کم می‌شوند.
+            self.save()
+            c = Customer.objects.get(id = self.customer_id )    # ول آن از حساب مشتری کاسته شده
+            c.balance -= self.total_price
+            c.save()
+
+    def cancel(self):       # از سفارش خود انصراف داده و اعتبار خود را پس بگیرد. در این صورت وضعیت سفارش به «لغوشده» تغییر کرده و کالاها نیز به موجودی فروشگاه برمی‌گردند.
         pass
+        # self.status = self.STATUS_CANCELED
+        # self.save()
 
     def send(self):
         pass
